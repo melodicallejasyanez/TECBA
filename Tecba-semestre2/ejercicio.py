f4a469e8d1a26b155184b04be7d5a6a89abd3711
Melodi <melodicallejas.sis23ch@tecba.edu.bo>
2024-10-24 15:13:34 -0400

    Java Scrip

---
+++ Tecba-semestre2/ejercicio.py
@@ -0,0 +1,8 @@
+nombre = "mel" #strinf "cadena"
+bandera = true #booleano
+numero = 10 #int enteroS
+
+print("hola", nombre)
+
+if bandera == true;
+    
---
+++ Tecba-semestre2/js/app.js
@@ -0,0 +1,41 @@
+//let alacena variables
+//var no se usa
+//const almecena variables unicas
+
+//let nombre = "mel"
+
+//let nombre = `Mel`;
+//console.log('Hola Mundo', nombre);
+
+//let numero = 10;
+//let bandera = false;
+//let texto = "Hola Mundo";
+
+//console.log(texto.toUpperCase());
+
+//if (numero == 10) {
+    //console.log("fleicidades");
+//}else{
+    //console.log("Intentalo de nuevo");
+//}
+
+//EJERCICO 1: NUMERO PARA O IMPAR
+let numero = 45
+
+if (numero%2 == 0) {
+    console.log("El numero es par");
+}else{
+    console.log("El numero es impar");
+}
+
+
+//EJERCICIO 2: NUMERO EN UN RANGO DE 10 A 15
+
+let numero = 12
+
+if (numero >= 10 & numero <= 15) {
+    console.log("El numero esta en el rango");
+}else{
+    console.log("El numero no esta en el ranngo");
+}
+
